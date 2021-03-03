173b7e02da414ff30bdf71f94619c2c04503a652
Aigul <sharipova.aig@gmail.com>
2021-03-03 00:16:23 -0500

    html

--- js/script.js
+++ js/script.js
@@ -4,21 +4,38 @@
 
 const BASE_URL = "https://www.superheroapi.com/api.php/10222670862712756/search/wonder";
 
+// $('#exampleModal').modal('show');
+
 /*----- app's state (variables) -----*/
+let superheroData;
+
 /*----- cached element references -----*/
+const $collection = $('#collection');
 /*----- event listeners -----*/
 /*----- functions -----*/
 
-init();
+// init();
 
-function init() {
+// function init() {
+  // getData();
+// }
+$('.card').click(function () {
+  console.log('keke');
   getData();
-}
+});
 function getData(){
   $.ajax(BASE_URL)
   .then(function(data) {
     console.log('data: ', data);
+    superheroData = data;
+    $('#exampleModal').modal('show');
+
+    // render();
   }, function(error) {
       console.log('error: ', error);
   });
+}
+
+function render() {
+  // superheroData.result.map(function);
 }
